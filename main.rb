2488797139f4a9bc1a399aef221026dac49adfe1
d2gher <d2gher@gmail.com>
2023-02-21 15:18:48 +0200

    Add option to load savefile

--- main.rb
+++ main.rb
@@ -9,4 +9,19 @@ word_in_progress = Array.new(word.length, '_').join.to_s
 wrong_letters = []
 currect_letters = []
 
+files = Dir['savefiles/*']
+files.each_with_index do |file, index|
+  puts "#{index + 1}. #{file.gsub('savefiles/', '')}"
+end
+
+puts 'Enter the number of file to load:'
+filenumber = gets.chomp
+
+file = eval(File.read(files[filenumber.to_i - 1]))
+
+word = file[:word]
+word_in_progress = file[:word_in_progress]
+wrong_letters = file[:wrong_letters]
+currect_letters = file[:currect_letters]
+
 play_a_round(word, word_in_progress, wrong_letters, currect_letters)
